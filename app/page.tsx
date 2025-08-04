d1dfb06dac8d273e0571df8608c6921d6121b646
Gaurav Kavat <gauravkavat@gmail.com>
2025-08-04 21:34:33 +0530

    Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { TextHoverEffect } from "@/components/ui/text-hover-effect";
-
 export default function Home() {
   return (
-    <div className="h-[30rem] flex items-center justify-center">
-      <TextHoverEffect text="DevTasker" />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300">
+      <h1 className="text-4xl font-bold mb-4">
+        Welcome to Dev-Tasker
+      </h1>
+      <p className="mt-4">
+        This is a task management app designed for developers.
+      </p>
     </div>
   );
 }
