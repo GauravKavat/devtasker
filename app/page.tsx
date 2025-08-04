051737b375765233db91c161a0c57499af0e6c58
Gaurav Kavat <gauravkavat@gmail.com>
2025-08-04 21:54:07 +0530

    base

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
       <h1 className="text-4xl font-bold mb-4">
         Welcome to Dev-Tasker
       </h1>
