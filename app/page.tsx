244f18744f3ae1f46ef5b5b983068b7d8d045a90
Gaurav Kavat <gauravkavat@gmail.com>
2025-08-06 23:12:02 +0530

    coming-soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import { TextScroll } from "@/components/ui/text-scroll";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
-      <h1 className="text-4xl font-bold mb-4">
-        Welcome to Dev-Tasker
-      </h1>
-      <p className="mt-4">
-        This is a task management app designed for developers.
-      </p>
+      <TextScroll
+      className="font-display text-center text-4xl font-semibold tracking-tighter  text-black dark:text-white md:text-7xl md:leading-[5rem]"
+      text="Coming Soon  "
+      default_velocity={1}
+    />
     </div>
   );
 }
