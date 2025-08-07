5eb48e05d2f8038b0f9e5ff8d167accf75bf6fcc
Gaurav Kavat <gauravkavat@gmail.com>
2025-08-07 11:27:40 +0530

    coming-soon

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
       <TextScroll
-      className="font-display text-center text-4xl font-semibold tracking-tighter  text-black dark:text-white md:text-7xl md:leading-[5rem]"
-      text="Coming Soon  "
+      className="font-display text-center text-4xl font-semibold tracking-tighter text-black dark:text-white md:text-7xl md:leading-[5rem]"
+      text="{ DEV }  "
       default_velocity={1}
     />
     </div>
