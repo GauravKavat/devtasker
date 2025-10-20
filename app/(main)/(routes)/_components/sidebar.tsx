79ee95af14ed551171e60bf49c077d4815a6b881
Gaurav Kavat <gauravkavat@gmail.com>
2025-10-20 23:27:55 +0530

    Add Sidebar component to Dashboard for improved layout

---
+++ app/(main)/(routes)/_components/sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Sidebar() {
+  return (
+    <div className="flex flex-col h-screen w-[15%] border-r border-gray-200 dark:border-gray-700">
+      <h1>SideBar</h1>
+    </div>
+  );
+}
--- app/(main)/(routes)/dashboard/page.tsx
+++ app/(main)/(routes)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
+import Sidebar from "@/app/(main)/(routes)/_components/sidebar";
+
 export default async function Dashboard() {
   const { userId } = await auth();
 
@@ -9,8 +11,11 @@ export default async function Dashboard() {
   }
 
   return (
-    <div>
-      <h1>Welcome to Dashboard</h1>
+    <div className="flex flex-row gap-x-4 w-full">
+      <Sidebar />
+      <div className="flex items-center">
+        <h1>Welcome to Dashboard</h1>
+      </div>
     </div>
   );
 }
